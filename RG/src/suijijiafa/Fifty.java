c099b65b2e00b171a2fb484b64e735aada8f275c
li1243893836 <56986669+li1243893836@users.noreply.github.com>
2019-11-12 21:30:20 +0800

    New version

--- RG/src/suijijiafa/Fifty.java
+++ RG/src/suijijiafa/Fifty.java
@@ -1,71 +1,60 @@
 package suijijiafa;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Fifty {
 
 	static int a;static int b;		//记录随机数
 	static char c;				//记录符号
-	static String []d = new String[51];		//记录算式
+//	static String []d = new String[51];		//记录算式
+	Set<String> set = new HashSet();		//记录无重复算式
 	static int []e = new int[51];		//记录结果
-	public void suiji(int i) {	//产生随机数，并记录
-		do {
-			a = (int)(Math.random()*100);
-			b = (int)(Math.random()*100);
-		}while((a+b>100)||(a-b<0));
-		if((int)(Math.random()*100)%2==0) {		//控制加减法
-			c='+';
-			e[i]=a+b;
-			d[i]=a+"+"+b;
-		}
-		else {
-			c='-';
-			e[i]=a-b;
-			d[i]=a+"-"+b;
-		}
-	}
-	
-	public Boolean check(String d[],int i) {	//检查是否符合要求
-		int j;
-		for( j = 1;j<i;j++) {
-			if(d[j]==d[i]) {
-				break;
+	public void suiji() {	//产生随机数，并记录
+		for(int i = 1;i <= 50;i++) {
+			do {
+					a = (int)(Math.random()*100);
+					b = (int)(Math.random()*100);
+				}while((a + b > 100) || (a - b < 0));
+			if((int)(Math.random()*100)%2 == 0) {		//控制加减法
+				c = '+';
+				e[i] = a + b;
+				set.add(a+"+"+b);
+			}
+			else {
+				c = '-';
+				e[i] = a - b;
+				set.add(a+"-"+b);
 			}
 		}
-		if(i!=j) {
-			return false;
-		}
-		return true;
 	}
 	
-	public void output(int i) {		//输出算式
-		
-		System.out.print(d[i]+"="+"\t");
-		if(i%5==0) {
-			System.out.println();
+	
+	public void output() {		//输出算式
+		int i = 0;
+		for(Object object:set) {
+			i++;
+			System.out.print(object + "=" + "\t");
+			if(i % 5 == 0) {
+				System.out.println();
+			}
 		}
 	}
 	
-	public void out(int i) {			//输出结果
-		
-		System.out.println("第"+i+"道题答案为："+e[i]);
+	public void out() {			//输出结果
+		for(int i = 1;i <= 50; i++ ) {
+			System.out.println("第"+i+"道题答案为："+e[i]);
+		}
 	}
 	
 	
 	
 	
 	public static void main(String[] args) {
-		model50 ran = new model50();
-		for(int i=1;i<=50;i++) {
-			if(!ran.check(d, i)) {
-				i--;
-			}
-			else {
-				ran.suiji(i);
-				ran.output(i);
-			}
-			
-		}
-		for(int i=1;i<=50;i++) {
-			ran.out(i);
-		}
+		Fifty ran = new Fifty();
+		ran.suiji();
+		ran.output();
+		ran.out();
+		
 	}
 }
