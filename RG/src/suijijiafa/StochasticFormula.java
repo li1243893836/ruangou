789d6dd4291faea5af1845210860bda9dc8224e4
li1243893836 <56986669+li1243893836@users.noreply.github.com>
2019-11-12 22:34:10 +0800

    Modify method property name

---
+++ RG/src/suijijiafa/StochasticFormula.java
@@ -0,0 +1,62 @@
+package suijijiafa;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class StochasticFormula {
+	static int RandomNum1;static int RandomNum2;		//记录随机数
+	static char Symbol;				//记录符号
+//	static String []d = new String[51];		//记录算式
+	static Set<String> set = new LinkedHashSet();		//记录无重复算式  LinkedHashSet插入顺序
+	static int []Result = new int[51];		//记录结果
+	public void FormulaSet() {	//产生随机数，并记录
+		for(;set.size()<50;) {
+			int i = set.size();			//记录个数
+			do {
+				RandomNum1 = (int)(Math.random()*100);
+				RandomNum2 = (int)(Math.random()*100);
+				}while((RandomNum1 + RandomNum2 > 100) || (RandomNum1 - RandomNum2 < 0));
+			if((int)(Math.random()*100)%2 == 0) {		//控制加减法
+				Symbol = '+';
+				set.add(RandomNum1+"+"+RandomNum2);
+				Result[i] = RandomNum1 + RandomNum2;
+			}
+			else {
+				Symbol = '-';
+				set.add(RandomNum1 + "-" + RandomNum2);
+				Result[i] = RandomNum1 - RandomNum2;
+			}
+			
+		}
+	}
+	
+	
+	public void OutputFormula() {		//输出算式
+		int i = 0;
+		for(Object object:set) {
+			i++;
+			System.out.print(object + "=" + "\t\t");
+			if(i % 5 == 0) {
+				System.out.println();
+			}
+		}
+	}
+	
+	public void OutputResult() {			//输出结果
+		for(int i = 0;i < 50; i++ ) {
+			System.out.println("第" + (i + 1) + "道题答案为：" + Result[i]);
+		}
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		StochasticFormula ran = new StochasticFormula();
+		ran.FormulaSet();
+		ran.OutputFormula();
+		System.out.println("\t\t\t\t"+"****参考答案****"+"\t\t\t\t");
+		ran.OutputResult();
+		
+	}
+}
